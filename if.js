82406d04724f60d495decdb22c44088679ff5ef9
Vitālijs Korabeļņikovs <vitalijs.korabelnikovs@gmail.com>
2024-10-14 21:06:18 +0300

    test

--- if.js
+++ if.js
@@ -1,4 +1,4 @@
-let showText = true;
+let showText = false;
 let str1 = `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
 `;
 console.log(str1.length>10);
@@ -12,6 +12,8 @@ if(showText){
 
 
 // calculator
+
+document.write("<br>");
 
 let int1 = 30,int2 = 45;
 
@@ -39,5 +41,8 @@ function count(int1,int2,action){
 
 
 }
+
+
+count(30,67,"-");
 
 // calc + - * / 
